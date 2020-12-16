bb7d8cdf5da8d85668300fa135f6b361386c0404
Sherman Wood <sgwood63@users.noreply.github.com>
2020-12-16 15:10:13 -0800

    Update AMI
    remove AWS credentials

--- utilities/pyramid-AMI-install.sh
+++ utilities/pyramid-AMI-install.sh
@@ -37,5 +37,6 @@ ls -lsa /usr/src/pyramid
 rm -rf *.run
 rm -f $(find . -maxdepth 1 -type f -name "*.sh" ! -name "$this_file_name")
 
-find /root/.*history /home/*/.*history -exec rm -f {} \;
-find /root/.ssh/authorized_keys /home/*/.ssh/authorized_keys -exec rm -f {} \;
+sudo find /root/.*history /home/*/.*history -exec rm -f {} \;
+sudo find /root/.ssh/* /home/*/.ssh/* -exec rm -f {} \;
+sudo find /root/.aws /home/*/.aws -exec rm -rf {} \;
