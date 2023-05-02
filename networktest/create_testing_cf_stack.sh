2d7964b9e0b0e1e1b444aae446e8a4624019d62f
Lutz Paelike <lutz.palike@pyramidanalytics.com>
2023-05-02 09:58:18 +0200

    pass more parameters, add security group for the test instance, add permission for cleanup of the SG, remove some commands that perform network calls before our check runs

--- networktest/create_testing_cf_stack.sh
+++ networktest/create_testing_cf_stack.sh
@@ -1,8 +1,16 @@
 aws cloudformation create-stack \
-  --debug \
   --stack-name lp-cf-testing \
   --template-body file://$(pwd)/single_instance_with_networktest.template \
   --region us-east-1 \
-  --parameters "$(cat cf_testing_params.json)" \
-  --capabilities "CAPABILITY_IAM" "CAPABILITY_AUTO_EXPAND"
+  --parameters "$(cat cf_testing_params_priv.json)" \
+  --capabilities "CAPABILITY_IAM" "CAPABILITY_AUTO_EXPAND" \
+  # --debug \
 
+# arn:aws:cloudformation:us-east-1:343272018671:stack/lp-cf-testing-NetworkTestStack-IQ305P7IFCTK/*
+
+#        {
+#            "Sid": "VisualEditor3",
+#            "Effect": "Allow",
+#            "Action": "cloudformation:DeleteStack",
+#            "Resource": "arn:aws:cloudformation:us-east-1:343272018671:stack/lp-cf-testing-NetworkTestStack-IQ305P7IFCTK/*"
+#        },
